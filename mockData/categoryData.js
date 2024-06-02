f36b3c58023316f5deea375e54d0fc9f9b72d663
Adjei Rapheal <radjei119@st.ug.edu.gh>
2024-06-02 13:44:33 +0000

    Categories Added

---
+++ mockData/categoryData.js
@@ -0,0 +1,20 @@
+export const categoryData = [
+    {
+        id: 0,
+        name: "Exercise",
+        description: "12 Task",
+        image: require("../assets/woman1.png"),
+    },
+    {
+        id: 1,
+        name: "Study",
+        description: "12 Task",
+        image: require("../assets/woman.png"),
+      },
+      {
+        id: 2,
+        name: "Study",
+        description: "12 Task",
+        image: require("../assets/woman.png"),
+      },
+]
