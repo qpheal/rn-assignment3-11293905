86764c8f80f8485e9eb4bcb831fdb4714662d5cc
Adjei Rapheal <radjei119@st.ug.edu.gh>
2024-06-02 00:52:25 +0000

    Header

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet,Image, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{backgroundColor:"#F7F0E8", padding:30, flex:1 }}>
+    <ScrollView style={{marginTop:15}}>
+    
+      <View style={{gap: 20}}>
+      {/**Header */}
+      <View style={{display:"flex", flexDirection:"row", justifyContent:"space-between", alignItems:"center"}}>
+       <View>
+        <Text style={{fontWeight:700, fontSize:32}}>Hello, Dev</Text>
+        <Text style={{fontSize:12}}>14 tasks devs</Text>
+       </View>
+
+       <TouchableOpacity style={{backgroundColor:"white", borderRadius:50, padding:5}}>
+       <Image source={require("./assets/person.png")} />
+       </TouchableOpacity>
+      </View>
+
+
+      
+      </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
